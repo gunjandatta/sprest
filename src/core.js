5db8e21dbcf8ea9c9479c3d6468141146b549bf9
Gunjan Datta <Gunjan Datta>
2016-08-24 16:15:23 -0400

    Add core and web classes.

---
+++ src/core.js
@@ -0,0 +1,7 @@
+"use strict";
+var Core = (function () {
+    function Core() {
+    }
+    return Core;
+}());
+exports.Core = Core;
---
+++ src/web.ts
@@ -0,0 +1,5 @@
+import {Core} from './core.ts';
+
+export class Web extends Core {
+
+}
