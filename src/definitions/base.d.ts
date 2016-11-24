3ecfc49ec560ff1c868d700cc07e1926730efc7e
Gunjan Datta <me@dattabase.com>
2016-11-23 19:16:15 -0500

    Updated the intellisense.

--- src/definitions/base.d.ts
+++ src/definitions/base.d.ts
@@ -30,18 +30,12 @@ declare module $REST.Types {
          * @param callback - The method to be executed after the request completes.
          */
         execute(callback?:(...args) => any): IBase;
-        execute<IList>(callback?:(...args) => any): IList;
-        execute<ISite>(callback?:(...args) => any): ISite;
-        execute<IWeb>(callback?:(...args) => any): IWeb;
 
         /**
          * Method to execute the request.
          * @param waitFl - Flag to execute the request, after the previous requests have completed.
          */
         execute(waitFl:boolean): IBase;
-        execute<IList>(waitFl:boolean): IList;
-        execute<ISite>(waitFl:boolean): ISite;
-        execute<IWeb>(waitFl:boolean): IWeb;
 
         /**
          * Method to execute the request.
@@ -49,16 +43,10 @@ declare module $REST.Types {
          * @param waitFl - Flag to execute the request, after the previous requests have completed.
          */
         execute(callback:any, waitFl:boolean): IBase;
-        execute<IList>(callback:any, waitFl:boolean): IList;
-        execute<ISite>(callback:any, waitFl:boolean): ISite;
-        execute<IWeb>(callback:any, waitFl:boolean): IWeb;
 
         /**
          * Method to execute the request synchronously.
          */
         executeAndWait(): IBase;
-        executeAndWait<IList>(): IList;
-        executeAndWait<ISite>(): ISite;
-        executeAndWait<IWeb>(): IWeb;
     }
 }
