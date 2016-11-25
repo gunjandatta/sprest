403278edd2431f5c2ca581fe44799bbfecff314e
Gunjan Datta <me@dattabase.com>
2016-11-25 16:57:50 -0500

    Added more keywords.

--- test/test.ts
+++ test/test.ts
@@ -1,10 +1,1 @@
 /// <reference path="../dist/sprest.d.ts" />
-
-// TO DO
-let props = (new $REST.PeopleManager()).getMyProperties().execute();
-for(let prop of props.UserProfileProperties.results) {
-    prop.Key;
-}
-
-let up = (new $REST.ProfileLoader()).getUserProfile().execute();
-up.AccountName;
