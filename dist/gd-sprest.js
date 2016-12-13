d49b3068eab7390f07d778010e8a681b4d9d88d9
Gunjan Datta <me@dattabase.com>
2016-12-13 12:35:24 -0500

    Updated the context information class to include all of the properties from _spPageContextInfo.

--- dist/gd-sprest.js
+++ dist/gd-sprest.js
@@ -1543,9 +1543,69 @@ var $REST;
             configurable: true
         });
         ;
+        Object.defineProperty(ContextInfo, "alertsEnabled", {
+            // Alerts Enabled
+            get: function () { return this._contextInfo.alertsEnabled; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "allowSilverlightPrompt", {
+            // Allow Silverlight Prompt
+            get: function () { return this._contextInfo.allowSilverlightPrompt == "True" ? true : false; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "clientServerTimeDelta", {
+            // Client Server Time Delta
+            get: function () { return this._contextInfo.clientServerTimeDelta; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "crossDomainPhotosEnabled", {
+            // Cross Domain Photos Enabled
+            get: function () { return this._contextInfo.crossDomainPhotosEnabled; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "currentCultureName", {
+            // Current Culture Name
+            get: function () { return this._contextInfo.currentCultureName; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "currentLanguage", {
+            // Current Language
+            get: function () { return this._contextInfo.currentLanguage; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "currentUICultureName", {
+            // Current UI Culture Name
+            get: function () { return this._contextInfo.currentUICultureName; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "env", {
+            // Environment
+            get: function () { return this._contextInfo.env; },
+            enumerable: true,
+            configurable: true
+        });
         Object.defineProperty(ContextInfo, "existsFl", {
             // Exists Flag
             get: function () { return this._contextInfo.existsFl == null; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "hasManageWebPermissions", {
+            // Has Manage Web Permissions
+            get: function () { return this._contextInfo.hasManageWebPermissions; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "isAnonymousGuestUser", {
+            // Is Anonymous Guest User
+            get: function () { return this._contextInfo.isAnonymousGuestUser; },
             enumerable: true,
             configurable: true
         });
@@ -1555,9 +1615,57 @@ var $REST;
             enumerable: true,
             configurable: true
         });
+        Object.defineProperty(ContextInfo, "isSiteAdmin", {
+            // Is Site Administrator
+            get: function () { return this._contextInfo.isSiteAdmin; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "layoutsUrl", {
+            // Layouts Url
+            get: function () { return this._contextInfo.layoutsUrl; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "pageItemId", {
+            // Page Item Id
+            get: function () { return this._contextInfo.pageItemId; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "pageListId", {
+            // Page List Id
+            get: function () { return this._contextInfo.pageListId; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "pagePersonalizationScope", {
+            // Page Personalization Scope
+            get: function () { return this._contextInfo.pagePersonalizationScope; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "profileUrl", {
+            // Profile Url
+            get: function () { return this._contextInfo.profileUrl; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "serverRequestPath", {
+            // Server Request Path
+            get: function () { return this._contextInfo.serverRequestPath; },
+            enumerable: true,
+            configurable: true
+        });
         Object.defineProperty(ContextInfo, "siteAbsoluteUrl", {
             // Site Absolute Url
             get: function () { return this._contextInfo.siteAbsoluteUrl; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "siteClientTag", {
+            // Site Client Tag
+            get: function () { return this._contextInfo.siteClientTag; },
             enumerable: true,
             configurable: true
         });
@@ -1567,15 +1675,87 @@ var $REST;
             enumerable: true,
             configurable: true
         });
+        Object.defineProperty(ContextInfo, "systemUserKey", {
+            // System User Key
+            get: function () { return this._contextInfo.systemUserKey; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "tenantAppVersion", {
+            // Tenant App Version
+            get: function () { return this._contextInfo.tenantAppVersion; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "themeCacheToken", {
+            // Theme Cache Token
+            get: function () { return this._contextInfo.themeCacheToken; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "updateFromDigestPageLoaded", {
+            // Update From Digest Page Loaded
+            get: function () { return this._contextInfo.updateFromDigestPageLoaded; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "userId", {
+            // User Id
+            get: function () { return this._contextInfo.userId; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "userLoginName", {
+            // User Login Name
+            get: function () { return this._contextInfo.userLoginName; },
+            enumerable: true,
+            configurable: true
+        });
         Object.defineProperty(ContextInfo, "webAbsoluteUrl", {
             // Web Absolute Url
             get: function () { return this._contextInfo.webAbsoluteUrl; },
             enumerable: true,
             configurable: true
         });
+        Object.defineProperty(ContextInfo, "webLanguage", {
+            // Web Language
+            get: function () { return this._contextInfo.webLanguage; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "webLogoUrl", {
+            // Web Logo Url
+            get: function () { return this._contextInfo.webLogoUrl; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "webPermMask", {
+            // Web Permissions Mask
+            get: function () { return this._contextInfo.webPermMask; },
+            enumerable: true,
+            configurable: true
+        });
         Object.defineProperty(ContextInfo, "webServerRelativeUrl", {
             // Web Server Relative Url
             get: function () { return this._contextInfo.webServerRelativeUrl; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "webTemplate", {
+            // Web Template
+            get: function () { return this._contextInfo.webTemplate; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "webTitle", {
+            // Web Title
+            get: function () { return this._contextInfo.webTitle; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ContextInfo, "webUIVersion", {
+            // Web UI Version
+            get: function () { return this._contextInfo.webUIVersion; },
             enumerable: true,
             configurable: true
         });
