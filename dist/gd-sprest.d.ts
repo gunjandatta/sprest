7cef08ed1c40217f77865813e90f049d7a689ce2
Gunjan Datta <me@dattabase.com>
2016-12-14 00:27:27 -0500

    Updated the intellisense for the people picker search/resovlve results.

--- dist/gd-sprest.d.ts
+++ dist/gd-sprest.d.ts
@@ -4711,6 +4711,29 @@ declare module $REST.Types {
 }
 declare module $REST.ComplexTypes {
     /**
+     * Entity Data
+     */
+    interface EntityData {
+        /** Department. */
+        Department: string;
+
+        /** EMail */
+        EMail: string;
+
+        /** */
+        IsAltSecIdPresent: boolean;
+
+        /** Mobile Phone */
+        MobilePhone: string;
+
+        /** Object ID */
+        ObjectId: string;
+
+        /** Title */
+        Title: string;
+    }
+
+    /**
      * Client People Picker Query Parameters
      */
     interface PeoplePickerQuery {
@@ -4759,6 +4782,44 @@ declare module $REST.ComplexTypes {
         /** The web application id. */
         WebApplicationID: any;
     }
+
+    /**
+     * People Picker Resolve User
+     */
+    interface PeoplePickerResolveUser {
+    }
+
+    /**
+     * People Picker Search User
+     */
+    interface PeoplePickerSearchUser {
+        /** The email address of the user. */
+        Description: string;
+
+        /** The full name of the user. */
+        DisplayText: string;
+
+        /** The user entity data. */
+        EntityData: EntityData;
+
+        /** Entity Type */
+        EntityType: string;
+
+        /** Flag to determine if the search user is resolved. */
+        IsResovled: boolean;
+
+        /** The user identity. */
+        Key: string;
+
+        /** Multiple Matches */
+        MultipleMatches: Array<any>;
+
+        /** Provider Display Name */
+        ProviderDisplayName: string;
+
+        /** Provider Name */
+        ProviderName: string;
+    }
 }
 
 declare module $REST.Types {
@@ -4785,6 +4846,22 @@ declare module $REST.Types {
          * @param query - The people picker query.
         */
         clientPeoplePickerSearchUser(query:ComplexTypes.PeoplePickerQuery);
+    }
+
+    /**
+     * People Picker Resolve User
+     */
+    interface IPeoplePickerResolveUser extends IBase {
+        // The client people picker resolve user results
+        ClientPeoplePickerResolveUser: Array<ComplexTypes.PeoplePickerResolveUser>;
+    }
+
+    /**
+     * People Picker Search User
+     */
+    interface IPeoplePickerSearchUser extends IBase {
+        // The client people picker search user results
+        ClientPeoplePickerSearchUser: Array<ComplexTypes.PeoplePickerSearchUser>;
     }
 }
 declare module $REST.Types {
