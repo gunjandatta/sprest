784498eb8139a1465e48103ffdc135d600d95494
Gunjan Datta <me@dattabase.com>
2017-11-17 17:36:44 -0500

    Updated the build process.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,13 @@
+var dts = require("dts-bundle");
 var path = require("path");
 var webpack = require("webpack");
-var dts = require("dts-bundle");
-var UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-
 // Set the export
-module.exports = function (env) {
-    // Set the production flag
-    var isProduction = env && env.isProd ? true : false;
+module.exports = env => {
+    let isProd = env && env.production;
 
-    // See if we are in production
-    if (isProduction) {
-        // Bundle the header files
+    // See if this is the production environment
+    if(isProd) {
+        // Bundle the header file
         dts.bundle({
             name: "gd-sprest",
             main: "build/gd-sprest.d.ts",
@@ -29,7 +26,7 @@ module.exports = function (env) {
         // Output
         output: {
             // Filename
-            filename: isProduction ? "gd-sprest.min.js" : "gd-sprest.js",
+            filename: isProd ? "gd-sprest.min.js" : "gd-sprest.js",
             // Folder
             path: path.join(__dirname, "dist")
         },
@@ -58,15 +55,6 @@ module.exports = function (env) {
             ]
         }
     };
-
-    // See if this is production
-    if (isProduction) {
-        // Plugins
-        cfg.plugins = [
-            // Minify the output
-            new UglifyJsPlugin()
-        ]
-    }
 
     // Return the configuration
     return cfg;
