446473c0193a240f357db2f52ed4478cdd83a4cd
Gunjan Datta <me@dattabase.com>
2018-02-01 23:49:32 -0500

    Fixed the intellisense for the save item method.

--- dist/gd-sprest.js
+++ dist/gd-sprest.js
@@ -11089,7 +11089,7 @@ var Mapper = __webpack_require__(12);
  * SharePoint REST Library
  */
 exports.$REST = {
-    __ver: 3.26,
+    __ver: 3.27,
     ContextInfo: Lib.ContextInfo,
     DefaultRequestToHostFl: false,
     Helper: {
--- src/rest.ts
+++ src/rest.ts
@@ -164,7 +164,7 @@ export interface IREST {
  * SharePoint REST Library
  */
 export const $REST: any = {
-    __ver: 3.26,
+    __ver: 3.27,
     ContextInfo: Lib.ContextInfo,
     DefaultRequestToHostFl: false,
     Helper: {
