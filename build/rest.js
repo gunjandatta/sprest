4c392521cbb496e59fcb2aa1db056d8d9d58e942
Gunjan Datta <me@dattabase.com>
2018-10-06 15:51:59 -0400

    Fixed a bug w/ the package file. The main property should point to the main "build" file not the dist file.

--- build/rest.js
+++ build/rest.js
@@ -7,7 +7,7 @@ var Mapper = require("./mapper");
  * SharePoint REST Library
  */
 exports.$REST = {
-    __ver: 4.29,
+    __ver: 4.30,
     AppContext: function (siteUrl) { return Lib.Site.getAppContext(siteUrl); },
     ContextInfo: Lib.ContextInfo,
     DefaultRequestToHostFl: false,
