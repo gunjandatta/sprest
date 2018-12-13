34ab8010a3c9ba426bd3f651ab26e61bfc5ff11b
Gunjan Datta <me@dattabase.com>
2018-12-13 10:58:09 -0500

    Updated the npm package configuration.

--- build/rest.js
+++ build/rest.js
@@ -7,7 +7,7 @@ var Mapper = require("./mapper");
  * SharePoint REST Library
  */
 exports.$REST = {
-    __ver: 4.43,
+    __ver: 4.44,
     AppContext: function (siteUrl) { return Lib.Site.getAppContext(siteUrl); },
     ContextInfo: Lib.ContextInfo,
     DefaultRequestToHostFl: false,
