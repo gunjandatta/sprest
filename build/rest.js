a37119b4c3873d3a8bb8fa1d1ada6fb49c46266c
Gunjan Datta <me@dattabase.com>
2019-08-22 18:42:34 -0400

    Updated the definitions to fix an intellisense issue w/ the Items.add() method.

--- build/rest.js
+++ build/rest.js
@@ -7,7 +7,7 @@ var Mapper = require("./mapper");
  * SharePoint REST Library
  */
 exports.$REST = {
-    __ver: 4.90,
+    __ver: 4.91,
     AppContext: function (siteUrl) { return Lib.Site.getAppContext(siteUrl); },
     Apps: Lib.Apps,
     ContextInfo: Lib.ContextInfo,
