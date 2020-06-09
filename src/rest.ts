9813e15aa821b4ab0357fbff8b99aeb5d40be485
Gunjan Datta <me@dattabase.com>
2020-06-09 10:44:59 -0400

    Updated the package reference for types.

--- src/rest.ts
+++ src/rest.ts
@@ -7,7 +7,7 @@ import { IREST } from "../@types";
  * SharePoint REST Library
  */
 export const $REST: IREST = {
-    __ver: 5.76,
+    __ver: 5.77,
     AppContext: (siteUrl: string) => { return Lib.Site.getAppContext(siteUrl); },
     Apps: Lib.Apps,
     ContextInfo: Lib.ContextInfo,
