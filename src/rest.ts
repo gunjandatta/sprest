99e2bdc1e08339b08672ff349070d951d8b93d98
Gunjan Datta <me@dattabase.com>
2020-07-22 18:17:41 -0400

    Updated the core intellisense lib.

--- src/rest.ts
+++ src/rest.ts
@@ -7,7 +7,7 @@ import { IREST } from "../@types";
  * SharePoint REST Library
  */
 export const $REST: IREST = {
-    __ver: 5.94,
+    __ver: 5.95,
     AppContext: (siteUrl: string) => { return Lib.Site.getAppContext(siteUrl); },
     Apps: Lib.Apps,
     ContextInfo: Lib.ContextInfo,
