7c06a85b1f1cbe9379e222b161318086540b2f7e
Gunjan Datta <me@dattabase.com>
2020-09-14 16:53:11 -0400

    Bug Fix - The batch requests was not completing the promise in some cases.

--- dist/gd-sprest.d.ts
+++ dist/gd-sprest.d.ts
@@ -5975,7 +5975,7 @@ declare module 'gd-sprest/utils/request' {
         parseXML(xml: string): IBase;
     
         /** Updates the data object. */
-        updateDataObject(base: IBase, isBatchRequest: boolean);
+        updateDataObject(base: IBase, isBatchRequest: boolean, batchCallback?: (batchResponses: Array<any>) => void);
     
         /** Validates the data collection results. */
         validateDataCollectionResults(base: IBase): PromiseLike<void>;
