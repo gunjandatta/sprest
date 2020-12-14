3f1a29676b81ea6a70bdeef5b99f3b4413c32bf9
Gunjan Datta <me@dattabase.com>
2020-12-14 16:04:59 -0500

    Updated the request digest to use the context information, used by SPFx projects.

--- src/rest.ts
+++ src/rest.ts
@@ -7,7 +7,7 @@ import { IREST } from "../@types";
  * SharePoint REST Library
  */
 export const $REST: IREST = {
-    __ver: 6.20,
+    __ver: 6.21,
     AppContext: (siteUrl: string) => { return Lib.Site.getAppContext(siteUrl); },
     Apps: Lib.Apps,
     ContextInfo: Lib.ContextInfo,
