7ecd5190d18c41fa1c977dcf2e4ca2e327e1f26a
Gunjan Datta <me@dattabase.com>
2020-12-16 19:46:21 -0500

    Updated the lookup query to do a manual sort. The lookup field type may not always be allowed in an OData query.

--- src/rest.ts
+++ src/rest.ts
@@ -7,7 +7,7 @@ import { IREST } from "../@types";
  * SharePoint REST Library
  */
 export const $REST: IREST = {
-    __ver: 6.23,
+    __ver: 6.24,
     AppContext: (siteUrl: string) => { return Lib.Site.getAppContext(siteUrl); },
     Apps: Lib.Apps,
     ContextInfo: Lib.ContextInfo,
