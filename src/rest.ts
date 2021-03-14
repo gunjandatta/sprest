b1b6abfac6446c136e7435ddb218e7e6f908e2f5
Gunjan Datta <me@dattabase.com>
2021-03-14 12:45:21 -0400

    Updated the def lib to fix the build error for the type definitions.

--- src/rest.ts
+++ src/rest.ts
@@ -7,7 +7,7 @@ import { IREST } from "../@types";
  * SharePoint REST Library
  */
 export const $REST: IREST = {
-    __ver: 6.39,
+    __ver: 6.40,
     AppContext: (siteUrl: string) => { return Lib.Site.getAppContext(siteUrl); },
     Apps: Lib.Apps,
     ContextInfo: Lib.ContextInfo,
