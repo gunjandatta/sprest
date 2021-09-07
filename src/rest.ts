dedc2fa4f25d8955c59eef23ce75826900ab245e
Gunjan Datta <me@dattabase.com>
2021-09-07 17:04:10 -0400

    Bug Fix - The ' needs to be replace with '' in some cases.

--- src/rest.ts
+++ src/rest.ts
@@ -7,7 +7,7 @@ import { IREST } from "../@types";
  * SharePoint REST Library
  */
 export const $REST: IREST = {
-    __ver: 6.56,
+    __ver: 6.57,
     AppContext: (siteUrl: string) => { return Lib.Site.getAppContext(siteUrl); },
     Apps: Lib.Apps,
     ContextInfo: Lib.ContextInfo,
