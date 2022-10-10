980d3de8fad9d25c76a8f6014d56725ef7eb2e63
Gunjan Datta <me@dattabase.com>
2022-10-10 14:33:33 -0400

    Updated the definitions.

--- src/rest.ts
+++ src/rest.ts
@@ -7,7 +7,7 @@ import { IREST } from "../@types";
  * SharePoint REST Library
  */
 export const $REST: IREST = {
-    __ver: 7.33,
+    __ver: 7.34,
     AppContext: (siteUrl: string) => { return Lib.Site.getAppContext(siteUrl); },
     Apps: Lib.Apps,
     ContextInfo: Lib.ContextInfo,
