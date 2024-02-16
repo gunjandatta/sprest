5e2acfc45734557fd2db123a2cf6b5d2b82b0053
Gunjan Datta <me@dattabase.com>
2024-02-16 16:37:21 -0500

    Updated the definitions.

--- test/test.ts
+++ test/test.ts
@@ -17,6 +17,7 @@ v2.sites.getList("Test").items().execute(items => {
     items.results[0].fields();
 });
 
+v2.sites().lists("").items();
 v2.sites().lists("293874-239478-238479-32847987").execute(list => {
     list.items().execute(items => {
         items.results[0].update({});
