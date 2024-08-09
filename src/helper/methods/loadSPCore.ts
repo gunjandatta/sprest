00de42e36c53133bc59232dd1eae7b37bdc0d559
Gunjan Datta <me@dattabase.com>
2024-08-09 13:26:54 -0400

    Updated the load core method to include sp.init.js.

--- src/helper/methods/loadSPCore.ts
+++ src/helper/methods/loadSPCore.ts
@@ -1,4 +1,5 @@
 import { IloadSPCore } from "../../../@types/helper/methods";
+import { Executor } from "../executor";
 
 /**
  * Loads the core SharePoint JavaScript library for JSOM.
@@ -7,34 +8,39 @@ export const loadSPCore: IloadSPCore = () => {
     // Return a promise
     return new Promise((resolve) => {
         // Define the core libraries to load
-        let libs = ["init", "MicrosoftAjax", "SP.Runtime", "SP"];
+        let libs = ["init", "MicrosoftAjax", "SP.Runtime", "SP", "SP.Init"];
 
         // Parse the libraries
-        for (let i = 0; i < libs.length; i++) {
-            let libName = libs[i];
+        let counter = 0;
+        Executor(libs, libName => {
+            // Return a promise
+            return new Promise(resolve => {
+                // See if a script already exists
+                if (document.querySelector("script[title='" + libName + "']") == null) {
+                    // Log
+                    console.debug("[gd-sprest] Loading the core library: " + libName);
 
-            // See if a script already exists
-            if (document.querySelector("script[title='" + libName + "']") == null) {
-                // Log
-                console.debug("[gd-sprest] Loading the core library: " + libName);
+                    // Create the script element to load the library
+                    let elScript = document.createElement("script");
+                    elScript.title = libName;
+                    elScript.src = document.location.origin + "/_layouts/15/" + libName + ".js";
 
-                // Create the script element to load the library
-                let elScript = document.createElement("script");
-                elScript.title = libName;
-                elScript.src = document.location.origin + "/_layouts/15/" + libName + ".js";
+                    // Wait for the previous library to load
+                    setTimeout(() => {
+                        // Load the script
+                        document.head.appendChild(elScript);
 
-                // Wait for the previous library to load
-                setTimeout(() => {
-                    // Load the script
-                    document.head.appendChild(elScript);
-                }, 10 * i);
-            } else {
-                // Log
-                console.debug("[gd-sprest] Core library already loaded: " + libName);
-            }
-        }
-
-        // Resolve the request
-        resolve();
+                        // Resolve the request
+                        resolve(null);
+                    }, 10 * counter++);
+                } else {
+                    // Log
+                    console.debug("[gd-sprest] Core library already loaded: " + libName);
+                }
+            }).then(() => {
+                // Resolve the request after all the libraries have been requested
+                resolve();
+            });
+        });
     });
 }
