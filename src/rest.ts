3dbb7e4cc268e245c6f91a615a2b9b3bb4319ecf
Gunjan Datta <me@dattabase.com>
2024-12-03 18:10:36 -0500

    Fixed a build error from the intellisense.

--- src/rest.ts
+++ src/rest.ts
@@ -8,7 +8,7 @@ import { IREST } from "../@types";
  * SharePoint REST Library
  */
 export const $REST: IREST = {
-    __ver: 8.47,
+    __ver: 8.48,
     AppContext: (siteUrl: string) => { return Lib.Site.getAppContext(siteUrl); },
     Apps: Lib.Apps,
     ContextInfo: Lib.ContextInfo,
