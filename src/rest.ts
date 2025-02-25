d091e8fe6cff5e892a02b9580651722f4631036a
Gunjan Datta <me@dattabase.com>
2025-02-25 14:30:39 -0500

    Updated the definitions. Adds missing "sensitivityLabel" property to the drive item.

--- src/rest.ts
+++ src/rest.ts
@@ -8,7 +8,7 @@ import { IREST } from "../@types";
  * SharePoint REST Library
  */
 export const $REST: IREST = {
-    __ver: 9.13,
+    __ver: 9.14,
     AppContext: (siteUrl: string) => { return Lib.Site.getAppContext(siteUrl); },
     Apps: Lib.Apps,
     ContextInfo: Lib.ContextInfo,
