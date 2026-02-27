80dba4c8586b1ec5a3be73a1c790b6c2b1825841
Gunjan Datta <me@dattabase.com>
2026-02-27 18:19:44 -0500

    Updated the logic for chaining methods to graph collection objects.

--- src/rest.ts
+++ src/rest.ts
@@ -8,7 +8,7 @@ import { IREST } from "../@types";
  * SharePoint REST Library
  */
 export const $REST: IREST = {
-    __ver: 9.54,
+    __ver: 9.55,
     AppContext: (siteUrl: string) => { return Lib.Site.getAppContext(siteUrl); },
     Apps: Lib.Apps,
     ContextInfo: Lib.ContextInfo,
