50f8913b1fd4c85c844683f83edd745bbb20bb3e
Gunjan Datta <me@dattabase.com>
2026-07-30 10:15:14 -0400

    Updated the callback query to look at the base targetInfo object.

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -751,11 +751,9 @@ export const Request = {
                 // Process the callbacks
                 Batch.processCallbacks(base.base.batchRequests[batchIdx]);
 
-                // See if the callback for odata query exists
-                if (base.targetInfo.callbackQuery) {
-                    // Call the method
-                    base.targetInfo.callbackQuery(base.base.batchRequests[batchIdx]);
-                }
+                // See if the callback for odata query exists and call the event
+                let callbackQuery = base.targetInfo.callbackQuery || base.base.targetInfo.callbackQuery;
+                callbackQuery ? callbackQuery(base.base.batchRequests[batchIdx]) : null;
             }
         }
     },
@@ -778,10 +776,8 @@ export const Request = {
                     base.nextFl = data["@odata.nextLink"] || (data.d && data.d.__next);
 
                     // See if the callback for odata query exists
-                    if (base.targetInfo.callbackQuery) {
-                        // Call the method
-                        base.targetInfo.callbackQuery(data.d?.results || data.value);
-                    }
+                    let callbackQuery = base.targetInfo.callbackQuery || base.base.targetInfo.callbackQuery;
+                    callbackQuery ? callbackQuery(data.d?.results || data.value) : null;
 
                     // See if there are more items to get
                     if (base.nextFl) {
